73be0c58cdf8775d5f414724a7906bea0f1bdd99
Ron Forbes <ronforbes528@gmail.com>
2014-10-27 18:04:53 +0900

    Added Block
    Created the Block game object and component with minimal functionality
    (position and type)

---
+++ BlockPartyClient/Assets/Scripts/Block.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+	public int X, Y;
+	public int Type;
+	public const int TypeCount = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+	public void Initialize(int x, int y, int type)
+	{
+		X = x;
+		Y = y;
+		Type = type;
+
+		transform.position = new Vector3(X, Y, 0.0f);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
