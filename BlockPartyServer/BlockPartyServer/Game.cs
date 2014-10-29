c8dc106dc0b95f86d35775c33c0c85d1c6c768e9
Ron Forbes <ronforbes528@gmail.com>
2014-10-29 15:14:00 +0900

    Created the server
    Created the barebones server

---
+++ BlockPartyServer/BlockPartyServer/Game.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace BlockPartyServer
+{
+	public class Game
+	{
+
+	}
+}
---
+++ BlockPartyServer/BlockPartyServer/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BlockPartyServer
+{
+	class Program
+	{
+		static readonly Game game = new Game();
+
+		public static void Main (string[] args)
+		{
+
+		}
+	}
+}
