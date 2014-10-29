3de8a6a59eb0fe73f58dc8e48016a75a4155983f
Ron Forbes <ronforbes528@gmail.com>
2014-10-30 00:55:17 +0900

    Splash and menu scenes
    Added a splash and menu scene. Tested transitions between them and
    existing scenes

---
+++ BlockPartyClient/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(Screen.width / 2 - 50.0f, Screen.height / 2 - 50.0f, 100.0f, 100.0f), "Play"))
+		{
+			Application.LoadLevel("Game");
+		}
+	}
+}
